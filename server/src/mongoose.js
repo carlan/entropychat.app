ac49dc33ac0f5ac89819ca13a0653d7901824b40
Carlan Calazans <carlan@users.noreply.github.com>
2020-07-24 23:02:32 +0100

    Updated issue fix docker mongo setup
    - Moved connection retries to server app
    -  Updated server Dockerfile to add extra parameter to pm2
    - Updated default.json to add extra parametes: reconnect_interval and reconnect_tries
    - Updated docker-compose to add healthchecks
    - Updated sample file to add extra parameters: MONGO_RECONNECT_INTERVAL and MONGO_RECONNECT_TRIES

--- server/src/mongoose.js
+++ server/src/mongoose.js
@@ -5,14 +5,86 @@ module.exports = function (app) {
   const mongodb = app.get('mongodb');
   const password = encodeURIComponent(mongodb.root_password);
   const connectionString = `mongodb://${mongodb.root_user}:${password}@${mongodb.host}:${mongodb.port}/${mongodb.db_name}?authSource=admin`;
+
+  const options = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    reconnectInterval: mongodb.reconnect_interval,
+    reconnectTries: mongodb.reconnect_tries,
+  };
+
+  const connState = {
+    isFirstConnection: true,
+    reconnectTries: 0
+  };
+
+  const gracefullyShutdown = () => {
+    mongoose.connection.close(() => {
+      console.warn('Received SIGTERM. Closing connection.');
+      process.exit(0);
+    });
+  };
+
   mongoose.connect(
     connectionString,
-    { useCreateIndex: true, useNewUrlParser: true }
-  ).catch(err => {
-    logger.error(err);
-    process.exit(1);
+    options
+  ).catch(err => {});
+
+  const conn = mongoose.connection;
+
+  conn.on('connecting', () => {
+    console.info('Connecting...');
   });
-  
+
+  conn.on('connected', () => {
+    console.info('Connected.');
+
+    connState.isFirstConnection = false;
+    connState.reconnectTries = 0;
+
+    if (process.send) {
+      process.send('ready');
+    }
+  });
+
+  conn.on('open', () => {
+    console.log('Connection opened.');
+  });
+
+  conn.on('reconnected', () => {
+    console.info('Connection reconnected.');
+  });
+
+  conn.on('disconnected', () => {
+    if (!connState.isFirstConnection) {
+      console.error(`Connection lost. Reconnecting in ${mongodb.reconnect_interval/1000} seconds.`);
+    }
+  });
+
+  conn.on('error', (err) => {
+    console.error('Connection error:', err.message);
+
+    if (connState.isFirstConnection) {
+      let connInterval = setTimeout(() => {
+        conn.openUri(connectionString).catch(err => {});
+        connState.reconnectTries += 1;
+        console.info(`Reconnecting attempt ${connState.reconnectTries}/${mongodb.reconnect_tries} in ${mongodb.reconnect_interval/1000} seconds.`);
+
+        if (connState.reconnectTries === +mongodb.reconnect_tries) {
+          clearInterval(connInterval);
+          console.error('Maximum number of reconnections reached.');
+          process.exit(1);
+        }
+      }, mongodb.reconnect_interval);
+    }
+
+    mongoose.disconnect();
+  });
+
+  process.on('SIGTERM', gracefullyShutdown);
+  process.on('SIGINT', gracefullyShutdown);
+  process.on('SIGQUIT', gracefullyShutdown);
+
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
